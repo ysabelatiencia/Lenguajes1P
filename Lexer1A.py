7d516cdd8286843fa02c6fde8686dd3415151ebc
axelauza <axelauza31@gmail.com>
2019-06-26 19:32:43 -0500

    Nuevas reglas lexicas

--- Lexer1A.py
+++ Lexer1A.py
@@ -1,25 +1,29 @@
 import ply.lex as lex
-
+import ply.yacc as yacc
 reserved = {
     'if': 'IF',
     'then': 'THEN',
     'else': 'ELSE',
     'while': 'WHILE',
-    "not":"NOT"
+    "not":"NOT",
+    'for':'FOR',
+    'print':'PRINT'
 }
 
 
-tokens=["VARIABLE","NUMBER","PLUS","MINUS","TIMES","DIVIDE","EQUALS","STRING","COMPARACION","ID"] + list(reserved.values())
+tokens=["VARIABLE","NUMBER","PLUS","MINUS","TIMES","DIVIDE","EQUALS","STRING","COMPARACION","ID","FINAL",
+        "COMMENT","NEWLINE"] + list(reserved.values())
 t_ignore=" \t| |( )*"
 t_PLUS=r"\+"
 t_MINUS=r"-"
 t_TIMES=r"\*"
 t_DIVIDE=r"/"
 t_EQUALS=r"="
+t_FINAL=r":"
 t_VARIABLE=r"[a-zA-Z]([a-zA-Z0-9])*"
 
 t_STRING=r"'([a-zA-Z0-9]| )*'"
-t_COMPARACION=r"=="
+t_COMPARACION=r"("+t_EQUALS+t_EQUALS+")|(>"+t_EQUALS+")|(<"+t_EQUALS+")"
 
 
 def t_ID(t):
@@ -39,8 +43,19 @@ def t_NUMBER(t):
     t.value=int(t.value)
     return t
 
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+    # No return value. Token discarded
+
+# Define a rule so we can track line numbers
+def t_NEWLINE(t):
+     r'\n+'
+     t.lexer.lineno += len(t.value)
+
 lex.lex()
-lex.input("if not 2332 =='carro'")
+lex.input("if not 2332 =='carro':\n121#sada")
 while True:
     tok=lex.token()
     if not tok: break
